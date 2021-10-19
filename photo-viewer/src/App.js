3bb0f39ad3e15e9dc0d64e896c47def3b0e01c03
Jordan <jordan.d.carlton@gmail.com>
2021-10-19 13:48:38 +0100

    Init image selector

--- photo-viewer/src/App.js
+++ photo-viewer/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PhotoViewer from "./PhotoViewer/PhotoViewer";
+import ImageSelector from "./ImageSelector/ImageSelector";
 
 const App = () => {
   return (
       <div className={"App"}>
         <h1>React Photo Viewer</h1>
-        <PhotoViewer src={"https://picsum.photos/id/600/1600/900.jpg"}/>
+          <PhotoViewer src={"https://picsum.photos/id/600/1600/900.jpg"} />
+          <ImageSelector />
       </div>
   )
 }
---
+++ photo-viewer/src/ImageSelector/ImageSelector.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import { imageUrls } from "./ImageUrls";
+import "./ImageSelector.css"
+
+const ImageSelector = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ImageSelector
