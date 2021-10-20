17205281f4cde7b033d0b40fcc6cb172ca2cbc4f
Jordan <jordan.d.carlton@gmail.com>
2021-10-20 11:44:32 +0100

    Add ImageUrls.test.js

--- photo-viewer/src/ImageSelector/ImageUrls.test.js
+++ photo-viewer/src/ImageSelector/ImageUrls.test.js
@@ -4,3 +4,7 @@ import {imageUrls} from "./ImageUrls";
 test('first link is correct', () => {
     expect(imageUrls[0]).toBe("https://picsum.photos/id/600/1600/900.jpg")
 });
+
+test('doesnt contain broken links', () => {
+    expect(imageUrls).not.toContain("https://picsum.photos/id/601/1600/900.jpg" && "https://picsum.photos/id/624/1600/900.jpg")
+})
