f6d36273efa4caf5125254373b6eb73e7b561060
Jordan <jordan.d.carlton@gmail.com>
2021-10-20 13:59:28 +0100

    add test

---
+++ photo-viewer/src/ImageSelector/ImageSelector.test.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+import { generateImage } from 'jsdom-screenshot'
+import {render} from "@testing-library/react";
+import ImageSelector from "./ImageSelector";
+import "../setupTests"
+
+it('has no visual regressions', async () => {
+    
+    render(<ImageSelector/>);
+    const screenshot = await generateImage()
+    expect(screenshot).toMatchImageSnapshot();
+});
