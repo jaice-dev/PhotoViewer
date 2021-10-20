cab69800839dc5dbc749fb9c973b9f566b9392f1
Jordan <jordan.d.carlton@gmail.com>
2021-10-20 09:30:16 +0100

    Add responsive column sizes

--- photo-viewer/src/ImageSelector/ImageSelector.js
+++ photo-viewer/src/ImageSelector/ImageSelector.js
@@ -6,18 +6,15 @@ const ImageSelector = (props) => {
     
     const [selected, setSelected] = useState()
     
-    
-    
     return (
         <div className={"ImageSelector"}>
         {
             imageUrls.map((image, index) => {
                 return <img 
-                    onClick={() => { props.sendDataToParent(image); setSelected(index)}}
+                    onClick={() => { props.sendDataToParent(image); setSelected(index); }}
                     className={index === selected ? "selected ImageSelectorPhoto" : "ImageSelectorPhoto"} 
                     src={image} 
-                    alt={"Image selector picture"}
-                />
+                    alt={"Image selector picture"}/>
             })
         }
         </div>
